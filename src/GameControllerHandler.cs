39d89b532509ff687453a6fd1a65044017be72bd
AdamInTheOculus <a.ta.sinclair@gmail.com>
2017-01-07 22:31:11 -0500

    Added controller management source/config files to src directory

---
+++ src/GameControllerHandler.cs
@@ -0,0 +1,127 @@
+﻿/// <summary>
+/// Game Controller Handler takes care of the various operating systems issue.
+/// The class will detect which OS is running, and adjust the controller input accordingly.
+/// </summary>
+/// 
+/// http://wiki.unity3d.com/index.php?title=Xbox360Controller
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerHandler : MonoBehaviour {
+
+	private System.String OperatingSystem;
+
+	// Buttons to represent input names
+	private string buttonA, buttonB, buttonX, buttonY;
+	private string buttonSelect, buttonStart;
+	private string buttonRightBumper, buttonLeftBumper;
+	private string buttonLeftStickClick, buttonRightStickClick;
+	private string axisLeftAnalogX, axisLeftAnalogY, axisRightAnalogX, axisRightAnalogY;
+	private string axisLeftTrigger, axisRightTrigger;
+
+	// Properties to access private buttons
+	public string ButtonA { get { return this.buttonA; } }
+	public string ButtonB { get { return this.buttonB; } }
+	public string ButtonX { get { return this.buttonX; } }
+	public string ButtonY { get { return this.buttonY; } }
+	public string ButtonSelect { get { return this.buttonSelect; } }
+	public string ButtonStart { get { return this.buttonStart; } }
+	public string ButtonRightBumper { get { return this.buttonRightBumper; } }
+	public string ButtonLeftBumper { get { return this.buttonLeftBumper; } }
+	public string ButtonLeftStickClick { get { return this.buttonLeftStickClick; } }
+	public string ButtonRightStickClick { get { return this.buttonRightStickClick; } }
+	public string AxisLeftAnalogX { get { return this.axisLeftAnalogX; } }
+	public string AxisLeftAnalogY { get { return this.axisLeftAnalogY; } }
+	public string AxisRightAnalogX { get { return this.axisRightAnalogX; } }
+	public string AxisRightAnalogY { get { return this.axisRightAnalogY; } }
+	public string AxisLeftTrigger { get { return this.AxisLeftTrigger; } }
+	public string AxisRightTrigger { get { return this.axisRightTrigger; } }
+
+	private void Start()
+	{
+		this.OperatingSystem = SystemInfo.operatingSystem;
+		InitializeController ();
+	}
+
+	/// <summary>
+	/// Determines what OS is running and calls platform-specific Initialize function.
+	/// </summary>
+	private void InitializeController()
+	{
+		if (OperatingSystem.Contains ("Mac")) {
+			Debug.Log ("Mac OS.");
+			InitializeForMac ();
+		} else if (OperatingSystem.Contains ("Windows")) {
+			Debug.Log ("Windows OS.");
+			InitializeForWindows ();
+		} else if (OperatingSystem.Contains ("Linux")) {
+			Debug.Log ("Linux OS.");
+			InitializeForLinux ();
+		} else {
+			Debug.Log ("Unknown OS.");
+		}
+	}
+
+	/// <summary>
+	/// Maps input buttons for controller on MacOS systems.
+	/// </summary>
+	private void InitializeForMac()
+	{
+		// Map all buttons to 'Settings > Input' names.
+		buttonA = "MacButtonA";
+		buttonB = "MacButtonB";
+		buttonX = "MacButtonX";
+		buttonY = "MacButtonY";
+		buttonSelect = "MacButtonSelect";
+		buttonStart = "MacButtonStart";
+		buttonRightBumper = "MacButtonRightBumper";
+		buttonLeftBumper = "MacButtonLeftBumper";
+		buttonRightStickClick = "MacButtonRightStickClick";
+		buttonLeftStickClick = "MacButtonLeftStickClick";
+
+		// Map all analog inputs to 'Settings > Input'.
+		axisLeftAnalogX = "MacAxisLeftAnalogX";
+		axisLeftAnalogY = "MacAxisLeftAnalogY";
+		axisRightAnalogX = "MacAxisRightAnalogX";
+		axisRightAnalogY = "MacAxisRightAnalogY";
+		axisLeftTrigger = "MacAxisLeftTrigger";
+		axisRightTrigger = "MacAxisRightTrigger";
+	}
+
+	/// <summary>
+	/// Maps input buttons for controller on Windows systems.
+	/// </summary>
+	private void InitializeForWindows()
+	{
+		// Map all buttons to 'Edit > Project Settings > Input' names.
+		buttonA = "WindowsButtonA";
+		buttonB = "WindowsButtonB";
+		buttonX = "WindowsButtonX";
+		buttonY = "WindowsButtonY";
+		buttonSelect = "WindowsButtonSelect";
+		buttonStart = "WindowsButtonStart";
+		buttonRightBumper = "WindowsButtonRightBumper";
+		buttonLeftBumper = "WindowsButtonLeftBumper";
+		buttonRightStickClick = "WindowsButtonRightStickClick";
+		buttonLeftStickClick = "WindowsButtonLeftStickClick";
+
+		// Map all analog inputs to 'Settings > Input'.
+		axisLeftAnalogX = "WindowsAxisLeftAnalogX";
+		axisLeftAnalogY = "WindowsAxisLeftAnalogY";
+		axisRightAnalogX = "WindowsAxisRightAnalogX";
+		axisRightAnalogY = "WindowsAxisRightAnalogY";
+		axisLeftTrigger = "WindowsAxisLeftTrigger";
+		axisRightTrigger = "WindowsAxisRightTrigger";
+	}
+
+	/// <summary>
+	/// Maps input buttons for controller on Linux systems.
+	/// TODO: Write support for Linux systems.
+	/// </summary>
+	private void InitializeForLinux()
+	{
+
+	}
+}
---
+++ src/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private GameControllerHandler controller;
+
+	// Controller input data
+	private float leftAnalogX, leftAnalogY;
+
+	// Player physics data
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<GameControllerHandler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleMovement ();
+		HandleActions ();
+	}
+
+	/// <summary>
+	/// This function executes once a frame. It handles all button presses made by player.
+	/// </summary>
+	private void HandleActions() {
+		if (Input.GetButtonDown (controller.ButtonA)) {
+			Debug.Log ("Player pressed A!");
+		}
+	}
+
+	/// <summary>
+	/// This function executes once a frame. It handles all analog stick movements made by player.
+	/// </summary>
+	private void HandleMovement() {
+		// Gives decimal value between -1 and 1
+		leftAnalogX = Input.GetAxis (controller.AxisLeftAnalogX);
+		leftAnalogY = Input.GetAxis (controller.AxisLeftAnalogY);
+
+		SmoothMovementHandling();
+		//RoughMovementHandling();
+
+		Debug.Log("Left Analog X: " + leftAnalogX);
+		Debug.Log ("Left Analog Y: " + leftAnalogY);
+		transform.position += (Vector3)velocity * Time.deltaTime;
+	}
+
+
+	/// <summary>
+	/// Smooths the movement by slowly accelerating and decelerating.
+	/// Almost like a "gliding effect".
+	/// </summary>
+	private void SmoothMovementHandling() {
+		var x = leftAnalogX * Time.deltaTime * 150.0f;
+		var z = leftAnalogY * Time.deltaTime * 10.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, -z);
+	}
+}
